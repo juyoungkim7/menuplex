eea05c51a4fdf043ae11778f249b841509f1ae31
jhwgnu <jhwon11@snu.ac.kr>
2017-01-18 12:09:40 +0900

    식당 추가

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from board.models import School, Meal
+from board.models import School, Meal, Restaurant
 # Register your models here.
 
 admin.site.register(School)
 admin.site.register(Meal)
+admin.site.register(Restaurant)
--- board/models.py
+++ board/models.py
@@ -7,11 +7,18 @@ class School(models.Model):
     def __str__(self):
         return self.school_name
 
+class Restaurant(models.Model):
+    school = models.ForeignKey(School)
+    rest_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.rest_name
+
 class Meal(models.Model):
     school = models.ForeignKey(School)
+    restaurant = models.ForeignKey(Restaurant)
     meal_name = models.CharField(max_length=200)
     price = models.IntegerField()
-
 
     def __str__(self):
         return self.meal_name
